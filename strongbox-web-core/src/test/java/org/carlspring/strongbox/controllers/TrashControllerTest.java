53a174b473384b993082d8250867773987935927
carlspring <carlspring@gmail.com>
2018-12-22 23:36:50 +0000

    SB-1323: Make the strongbox-web-core's tests work in parallel
    - Fixed the `TrashControllerTest`.
    - Fixed the `TrashControllerUndeleteTest`.

--- strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/TrashControllerTest.java
+++ strongbox-web-core/src/test/java/org/carlspring/strongbox/controllers/TrashControllerTest.java
@@ -8,16 +8,16 @@ import org.carlspring.strongbox.storage.repository.RepositoryDto;
 import org.carlspring.strongbox.yaml.configuration.repository.MavenRepositoryConfigurationDto;
 
 import javax.inject.Inject;
-import javax.xml.bind.JAXBException;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedHashSet;
-import java.util.Set;
 
 import io.restassured.module.mockmvc.response.ValidatableMockMvcResponse;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.http.HttpStatus;
@@ -26,6 +26,7 @@ import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 /**
  * @author Martin Todorov
@@ -33,6 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author Pablo Tirado
  */
 @IntegrationTest
+@Execution(CONCURRENT)
 public class TrashControllerTest
         extends MavenRestAssuredBaseTest
 {
